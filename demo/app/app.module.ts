4de607ef6871242f3440fd169581596694fe1e3d
olivier <olivier@evaletolab.ch>
2018-06-02 11:06:33 +0200

    fix use of exported module

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PrettyJsonModule } from 'angular2-prettyjson';
-import { UcWidgetModule } from '../../src/index';
+import { ngxUploadcareModule } from '../../src/index';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     PrettyJsonModule,
-    UcWidgetModule,
+    ngxUploadcareModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
