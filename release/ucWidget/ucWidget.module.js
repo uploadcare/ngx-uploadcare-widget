812a05141583e06dc64a7f3202105ab9b88d4370
Nikolay Zherdev <nickzherdev@gmail.com>
2018-08-10 12:44:52 +0300

    Build fix with "npm run package"

---
+++ release/ucWidget/ucWidget.module.js
@@ -0,0 +1,31 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { NgModule } from '@angular/core';
+import { UcWidgetComponent } from './ucWidget.component';
+import { UcWidgetCustomComponent } from '../ucWidgetCustom/ucWidgetCustom.component';
+var UcWidgetModule = /** @class */ (function () {
+    function UcWidgetModule() {
+    }
+    UcWidgetModule = __decorate([
+        NgModule({
+            declarations: [
+                UcWidgetCustomComponent,
+                UcWidgetComponent
+            ],
+            imports: [],
+            providers: [],
+            bootstrap: [],
+            exports: [
+                UcWidgetCustomComponent,
+                UcWidgetComponent
+            ],
+        })
+    ], UcWidgetModule);
+    return UcWidgetModule;
+}());
+export { UcWidgetModule };
+//# sourceMappingURL=ucWidget.module.js.map
---
+++ release/ucWidgetCustom/index.js
@@ -0,0 +1,2 @@
+export * from './ucWidgetCustom.component';
+//# sourceMappingURL=index.js.map
