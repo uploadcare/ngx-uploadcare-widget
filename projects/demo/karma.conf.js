cb92ed96d5e854f1624bc59e7785120dea4ea8c9
nd0ut <nd0ut.me@gmail.com>
2020-04-10 16:14:55 +0300

    fix tests for demo application

--- projects/demo/karma.conf.js
+++ projects/demo/karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--headless', '--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
